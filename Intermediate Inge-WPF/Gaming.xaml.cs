385642426ccefba42a067979f34aa165249f5965
Quicksoapy <mayafransen2003@gmail.com>
2022-09-20 09:54:38 +0200

    Made single method for handling click-objects. Scrapped cheems-mode.

--- Intermediate Inge-WPF/Gaming.xaml.cs
+++ Intermediate Inge-WPF/Gaming.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Timer = System.Timers.Timer;
 
 namespace Intermediate_Inge_WPF;
 
@@ -16,16 +18,12 @@ public partial class Gaming : Window
     /// </summary>
     bool isRed = false;
     private int pointCount = 0;
-    private object comboLock= new object();
+    private object comboLock = new object();
     private volatile int comboCount = 0;
     private int MaxComboCount = 0;
     
-    private bool localCheems = false;
     static Uri moleUri = new Uri("mole.png", UriKind.Relative);
     private BitmapImage moleImage = new BitmapImage(moleUri);
-    
-    static Uri cheemsBonkedUri = new Uri("cheemsbonked.png", UriKind.Relative);
-    private BitmapImage cheemsBonkedImg = new BitmapImage(cheemsBonkedUri);
     
     System.Timers.Timer delay = new System.Timers.Timer(2000);
     private System.Timers.Timer bonked = new Timer(1000);
@@ -33,21 +31,10 @@ public partial class Gaming : Window
     {
         InitializeComponent();
         
-        if (cheems)
-        {
-            moleUri = new Uri("cheemsmole.png", UriKind.Relative);
-            moleImage = new BitmapImage(moleUri);
-            localCheems = true;
-        }
-        
         delay.AutoReset = true;
         delay.Enabled = true;
         delay.Elapsed += TimerEvent;
         delay.Start();
-
-        string cursorPath = Directory.GetCurrentDirectory() + "\\cursor.cur";
-        Cursor myCur = new Cursor(cursorPath);
-        this.Cursor = myCur;
     }
 
     private void TimerEvent(object? sender, ElapsedEventArgs e)
@@ -103,7 +90,7 @@ public partial class Gaming : Window
                 }
                 
                 comboCount = 0;
-                delay.Interval = 2000;
+                delay.Interval = 1000;
                 Application.Current.Dispatcher.Invoke(new Action(() => {
                     MaxComboCounter.Content = MaxComboCount.ToString();
                     ComboCounter.Content = comboCount.ToString();
@@ -119,223 +106,25 @@ public partial class Gaming : Window
         }
     }
 
-    /*private void TopLeft_OnMouseMove(object sender, MouseEventArgs e)
+    private void Mole_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
     {
-        if (TopLeft.Background == Brushes.Red)
+        Canvas molePosition = (Canvas)sender;
+        if (molePosition.Background != Brushes.White)
         {
-            TopLeft.Background = Brushes.White;
-            isRed = false;
-            ComboCounter.Content = comboCount.ToString();
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval = 1000 / comboCount;
-            }
-            
-            pointCount += 1;
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void TopRight_OnMouseMove(object sender, MouseEventArgs e)
-    {
-        if (TopRight.Background == Brushes.Red)
-        {
-            TopRight.Background = Brushes.White;
-            isRed = false;
-            ComboCounter.Content = comboCount.ToString();
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval = 1000 / comboCount;
-            }
-            pointCount += 1;
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void BottomLeft_OnMouseMove(object sender, MouseEventArgs e)
-    {
-        if (BottomLeft.Background == Brushes.Red)
-        {
-            BottomLeft.Background = Brushes.White;
-            isRed = false;
-            ComboCounter.Content = comboCount.ToString();
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval = 1000 / comboCount;
-            }
-            pointCount += 1;
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void BottomRight_OnMouseMove(object sender, MouseEventArgs e)
-    {
-        if (BottomRight.Background == Brushes.Red)
-        {
-            BottomRight.Background = Brushes.White;
-            isRed = false;
-            
-            ComboCounter.Content = comboCount.ToString();
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval = 1000 / comboCount;
-            }
-            pointCount += 1;
-            PointCounter.Content = pointCount.ToString();
-        }
-    }*/
-
-    private void TopLeft_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (TopLeft.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                TopLeft.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(TopLeft);
-                bonked.Start();
-            }
-            TopLeft.Background = Brushes.White;
+            molePosition.Background = Brushes.White;
             isRed = false;
             lock (comboLock)
             {
                 comboCount += 1;
                 delay.Interval -= 100;
             }
-            
+
             pointCount += 1;
             ComboCounter.Content = comboCount.ToString();
             PointCounter.Content = pointCount.ToString();
         }
     }
 
-    private void TopRight_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (TopRight.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                TopRight.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(TopRight);
-                bonked.Start();
-            }
-            TopRight.Background = Brushes.White;
-            isRed = false;
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval -= 100;
-            }
-            
-            pointCount += 1;
-            ComboCounter.Content = comboCount.ToString();
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void BottomLeft_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (BottomLeft.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                BottomLeft.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(BottomLeft);
-                bonked.Start();
-            }
-            BottomLeft.Background = Brushes.White;
-            isRed = false;
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval -= 100;
-            }
-            pointCount += 1;
-            ComboCounter.Content = comboCount.ToString();
-            PointCounter.Content = pointCount.ToString();
-        }
-
-    }
-
-    private void BottomRight_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (BottomRight.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                BottomRight.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(BottomRight);
-                bonked.Start();
-            }
-            BottomRight.Background = Brushes.White;
-            isRed = false;
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval -= 100;
-            }
-            pointCount += 1;
-            ComboCounter.Content = comboCount.ToString();
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void TopMiddle_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (TopMiddle.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                TopMiddle.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(TopMiddle);
-                bonked.Start();
-            }
-            TopMiddle.Background = Brushes.White;
-            isRed = false;
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval -= 100;
-            }
-            pointCount += 1;
-            ComboCounter.Content = comboCount.ToString();
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-
-    private void BottomMiddle_OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (BottomMiddle.Background != Brushes.White)
-        {
-            if (localCheems)
-            {
-                BottomMiddle.Background = new ImageBrush(cheemsBonkedImg);
-                bonked.Enabled = true;
-                bonked.Elapsed += BonkedEvent(BottomMiddle);
-                bonked.Start();
-            }
-            BottomMiddle.Background = Brushes.White;
-            isRed = false;
-            lock (comboLock)
-            {
-                comboCount += 1;
-                delay.Interval -= 100;
-            }
-            pointCount += 1;
-            ComboCounter.Content = comboCount.ToString();
-            PointCounter.Content = pointCount.ToString();
-        }
-    }
-    
     private ElapsedEventHandler BonkedEvent(Canvas position)
     {
         position.Background = Brushes.White;
--- Intermediate Inge-WPF/obj/Debug/net6.0-windows/App.g.cs
+++ Intermediate Inge-WPF/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace Intermediate_Inge_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Intermediate_Inge_WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             Intermediate_Inge_WPF.App app = new Intermediate_Inge_WPF.App();
             app.InitializeComponent();
